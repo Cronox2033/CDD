04aa7f8bbdc0534f22fb8b7e88277e2b993fe12f
Jordan Porras <jordan.porras1@gmail.com>
2019-08-29 00:55:19 -0400

    Crear vector con pares

--- Programa.cpp
+++ Programa.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//vector< pair<double,double>> Polinomio;
+vector< pair<long double,long double>> Polinomio;
+vector<long double> Numeros;
 
 int main(int argc, char **argv)
 {
     long double aux = 0;
     string pol = argv[1], temp = "";
+    pair<long double, long double> par;
 
     /*El siguiente for transforma los datos del string en numeros long double
-    y los junta en pares, para añadirlo a un string*/
+    y los añade a un vector*/
     for (int i = 0; i < pol.length(); i++)
     {
         if(i==0 && pol[i] == '+')
@@ -27,13 +29,13 @@ int main(int argc, char **argv)
                 if (pol[i] == 'X' || pol[i] =='x')
                 {
                     aux = stold(temp);
-                    cout<<aux<<endl;
+                    Numeros.push_back(aux);
                     temp = "";
                 }
                 else if(pol[i] == '+' || pol[i] == '-')
                 {
                     aux = stold(temp);
-                    cout<<aux<<endl;
+                    Numeros.push_back(aux);
                     temp = "";
                     temp = temp + pol[i];
                 }
@@ -46,7 +48,29 @@ int main(int argc, char **argv)
         
     }
     aux = stold(temp);
-    cout<<aux<<endl;
+    Numeros.push_back(aux);
+
+    //Añade los numeros del vector antes creado a un vector de pares.
+    for (int i = 0; i < Numeros.size(); i+=2)
+    {
+       if((i+1)<Numeros.size())
+       {
+           par.first = Numeros[i];
+           par.second = Numeros[i+1];
+           Polinomio.push_back(par);
+       }
+       else
+       {
+           par.first = Numeros[i];
+           par.second = 0;
+           Polinomio.push_back(par);
+       }
+    }
+    
+    for (int i = 0; i < Polinomio.size(); i++)
+    {
+        cout<<Polinomio[i].first<< " "<<Polinomio[i].second<<endl;
+    }
     
 
     
