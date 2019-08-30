79aacb742ee1d9ed14395823095dc1a1ba2bacd3
Jordan Porras <jordan.porras1@gmail.com>
2019-08-30 19:39:31 -0400

    Mejora en los espacios, añadir comentarios y subir .exe

--- Programa.cpp
+++ Programa.cpp
@@ -3,16 +3,16 @@
 #define tope 10e-20
 using namespace std;
 
+//variables globales
 long double x = 0.5,fx = 0, dy = 0; //cambiar x para evaluar en otro numero
 vector< pair<long double,long double>> Polinomio;
 vector< pair<long double,long double>> Derivada;
 vector<long double> Numeros;
 
-void Derivar_Polinomio()
-{
+//Calcula la derivada del polinomio
+void Derivar_Polinomio(){
     pair<long double, long double> parDerivado;
-    for (int i = 0; i < Polinomio.size(); i++)
-    {
+    for (int i = 0; i < Polinomio.size(); i++)    {
         parDerivado.first = Polinomio[i].first*Polinomio[i].second;
         parDerivado.second = Polinomio[i].second - 1;
         if(parDerivado.second<0) parDerivado.second = 0;
@@ -21,11 +21,10 @@ void Derivar_Polinomio()
     }
 }
 
-void Evaluar_Funcion()
-{
+//Evalua la funcion y su derivada en x(variable global)
+void Evaluar_Funcion(){
     long double num = x,sumf = 0, sumd = 0;
-    for (int i = 0; i < Polinomio.size(); i++)
-    {
+    for (int i = 0; i < Polinomio.size(); i++)    {
         sumf = sumf + Polinomio[i].first*pow(num,Polinomio[i].second);
         sumd = sumd + Derivada[i].first*pow(num,Derivada[i].second);
     }
@@ -33,21 +32,18 @@ void Evaluar_Funcion()
     dy = sumd;
 }
 
-void Newton_raphson()
-{
-    long double x1 = 0,error = 0;
-    int cont = 0;
+//realiza la formula de Newton Raphson
+void Newton_raphson(){
     Derivar_Polinomio();
     Evaluar_Funcion();
-    if(dy == 0)
-    {
+    if(dy == 0){
         cout<<"La evaluacion de la derivada en: "<<x<<" da como resultado 0"<<endl;
     }
-    else
-    {
-        while(cont < iteraciones)
-        {
-            x1 = x - fx/dy;
+    else{
+        long double error = 0;
+        int cont = 0;
+        while(cont < iteraciones){
+            long double x1 = x - fx/dy;
             error = x1 - x;
             error = abs(error);
             x = x1;
@@ -55,17 +51,14 @@ void Newton_raphson()
             Evaluar_Funcion();
             cont+=1;
         }
-        if(error<= 0.005)
-        {
+        if(error<= 0.005){
             //Si la raiz es muy cercana a 0, se intercambia por 0
-            if(x>0 && x<tope)
-            {
+            if(x>0 && x<tope){
                 x = 0;
             }
             cout<<"Una de las raices del polinomio es: "<<x<<endl;
         }
-        else
-        {
+        else{
             cout<<"Despues de realizar 100 iteraciones"<<endl;
             cout<< "no se encontro una raiz con porcentaje de error menor a 0.5%"<<endl;
             cout<<"El valor de x despues de 100 iteraciones es: "<<x<<endl;
@@ -73,106 +66,84 @@ void Newton_raphson()
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     long double aux = 0;
     string pol = "",temp = "";
     pair<long double, long double> par;
 
     // Bugfix - si no se ingresa un argumento, se reemplaza por 0.
-    if (!argv[1])
-    {
+    if (!argv[1]){
         pol = "0", temp = "";
     }
-    else
-    {
+    else{
         pol = argv[1], temp = "";
     }
 
-    if(pol == "" || pol == " " || pol == "-" || pol == "+")
-    {
+    if(pol == "" || pol == " " || pol == "-" || pol == "+"){
         pol = "0";
     }
 
     /*El siguiente for transforma los datos del string en numeros long double
     y los añade a un vector*/
-    for (int i = 0; i < pol.length(); i++)
-    {
-        if(i==0 && pol[i] == '+')
-        {
+    for (int i = 0; i < pol.length(); i++){
+        if(i==0 && pol[i] == '+'){   //arreglar problema de inicio del polinomio con + o -
             temp = temp + pol[i];
         }
-        else if(i==0 && pol[i] == '-')
-        {
+        else if(i==0 && pol[i] == '-'){
             temp = temp + pol[i];
         }
-        else
-        {
-           if (pol[i] != ' ' && pol[i] != '*')
-            {
-                if (pol[i] == 'X' || pol[i] =='x')
-                {
-                    if(temp == "" || temp == "-" || temp == "+")
-                    {
+        else{
+           if (pol[i] != ' ' && pol[i] != '*'){
+                if (pol[i] == 'X' || pol[i] =='x'){
+                    if(temp == "" || temp == "-" || temp == "+"){
                         temp = temp + "1";
                         aux = stold(temp);
                         Numeros.push_back(aux);
                         temp = "";
                     }
-                    else
-                    {
+                    else{
                         aux = stold(temp);
                         Numeros.push_back(aux);
                         temp = "";
                     }
                 }
-                else if(pol[i] == '+' || pol[i] == '-')
-                {
-                    if(temp == "")
-                    {
+                else if(pol[i] == '+' || pol[i] == '-'){
+                    if(temp == ""){
                         temp = temp + "1";
                         aux = stold(temp);
                         Numeros.push_back(aux);
                         temp = "";
                         temp = temp + pol[i];
                     }
-                    else
-                    {
+                    else{
                         aux = stold(temp);
                         Numeros.push_back(aux);
                         temp = "";
                         temp = temp + pol[i];
                     }
                 }
-                else
-                {
+                else{
                     temp = temp + pol[i];
                 }
             } 
         }
-        
     }
-    if(pol.length()>1)
-    {
+    if(pol.length()>1){
     aux = stold(temp);
     Numeros.push_back(aux);
     }
 
     //Añade los numeros del vector antes creado a un vector de pares.
-    for (int i = 0; i < Numeros.size(); i+=2)
-    {
-       if((i+1)<Numeros.size())
-       {
+    for (int i = 0; i < Numeros.size(); i+=2){
+       if((i+1)<Numeros.size()){
            par.first = Numeros[i];
            par.second = Numeros[i+1];
            Polinomio.push_back(par);
        }
-       else
-       {
+       else{
            par.first = Numeros[i];
            par.second = 0;
-           if(pol[pol.length()-1] == 'x' || pol[pol.length()-1] == 'X')
-           {
+           if(pol[pol.length()-1] == 'x' || pol[pol.length()-1] == 'X'){
                par.second = 1;
            }
            Polinomio.push_back(par);
